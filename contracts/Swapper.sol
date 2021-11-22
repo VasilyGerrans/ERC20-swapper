dee17dd33fcd2dfde730fdbb2c03e5fb79f60373
VasilyGerrans <vgerrans@gmail.com>
2021-11-22 14:19:13 +0000

    New Swapper saves an array of routers and the default paths along whcih we expect the routers to trade

--- contracts/Swapper.sol
+++ contracts/Swapper.sol
@@ -6,38 +6,79 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Swapper {
     address private parent;
-    IUniswapV2Router02 private router;
 
-    constructor(address _parentAddress, address _routerAddress) {
+    IUniswapV2Router02[] public routers;
+    mapping(address => address[]) public routerPaths;
+
+    constructor(address _parentAddress, address[] memory _routerAddresses, address[][] memory _defaultPaths) {
+        require(_routerAddresses.length == _defaultPaths.length, "Swapper: Every router must have a path");
         parent = _parentAddress;
-        router = IUniswapV2Router02(_routerAddress);
+        for (uint256 i = 0; i < _routerAddresses.length; i++) {
+            addRouter(_routerAddresses[i], _defaultPaths[i]);
+        }
+    }
+
+    modifier onlyParent {
+        require(msg.sender == parent, "Swapper: Only parent can call this function");
+        _;
+    }
+
+    function addRouter(address routerAddress, address[] memory defaultPath) public onlyParent {
+        routers.push(IUniswapV2Router02(routerAddress));
+        routerPaths[routerAddress] = defaultPath;
+    }
+
+    function deleteRouter(address routerAddress) external onlyParent {
+        for (uint256 i = 0; i < routers.length; i++) {
+            if (routers[i] == IUniswapV2Router02(routerAddress)) {
+                for (uint256 j = i; j < routers.length - 1; j++) {
+                    routers[i] = routers[j + 1];
+                }
+                routers.pop();
+            }
+        }
+    }
+
+    function updatePath(address routerAddress, address[] memory newPath) external onlyParent {
+        routerPaths[routerAddress] = newPath;
+    }
+
+    function findOptimalRouter(uint256 amountIn) public view returns(IUniswapV2Router02, uint256, address[] memory) {
+        IUniswapV2Router02 bestRouter;
+        uint256 bestAmountOut;
+        for (uint256 i = 0; i < routers.length; i++) {
+            if (i == 0) {
+                bestRouter = routers[i];
+                uint256[] memory amounts = bestRouter.getAmountsOut(amountIn, routerPaths[address(routers[i])]);
+                bestAmountOut = amounts[amounts.length - 1];
+            } else {
+                uint256[] memory amountOut = routers[i].getAmountsOut(amountIn, routerPaths[address(routers[i])]);
+                uint256 possibleAmount = amountOut[amountOut.length - 1];
+                if (possibleAmount > bestAmountOut) {
+                    bestRouter = routers[i];
+                    bestAmountOut = possibleAmount;
+                }
+            }
+        }
+        return (bestRouter, bestAmountOut, routerPaths[address(bestRouter)]);
     }
 
     function swap(
-        address _tokenIn, 
-        address _tokenOut,
         uint256 _amountIn,
         uint256 _amountOutMin
-    ) external returns(uint256 amountOut) {
-        require(msg.sender == parent, "Swapper: Only parent can call this contract!");
-        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
-        IERC20(_tokenIn).approve(address(router), _amountIn);
-
-        // use direct path
-        address[] memory path;
-        path = new address[](2);
-        path[0] = _tokenIn;
-        path[1] = _tokenOut;
-
+    ) external onlyParent returns(uint256) {
+        (IUniswapV2Router02 router, uint256 amountOut, address[] memory path) = findOptimalRouter(_amountIn);
+        
+        IERC20(path[0]).transferFrom(msg.sender, address(this), _amountIn);
+        IERC20(path[0]).approve(address(router), _amountIn);
         router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
             _amountIn, 
             _amountOutMin, 
             path, 
-            address(this),
+            parent, // send funds directly to parent; parent must calculate amountOut itself
             block.timestamp
         );
 
-        amountOut = IERC20(_tokenOut).balanceOf(address(this));
-        IERC20(_tokenOut).transfer(msg.sender, amountOut);
+        return amountOut;
     }
 }
--- test/swapper-test.js
+++ test/swapper-test.js
@@ -4,14 +4,15 @@ const Web3 = require("web3");
 const abi = require("./ERC20ABI.json");
 
 describe("Swapper", function () {
-  let whale, Swapper, swapper, WMATIC, WBTC;
-  let whaleAddress = "0x01aeFAC4A308FbAeD977648361fBAecFBCd380C7"; // big boy on Polygon
-  let sushiRouter = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
-  let amount = Web3.utils.toWei("1000", "ether");
+  let whale, Swapper, swapper, WMATIC, WBTC, WETH;
+  const whaleAddress = "0x01aeFAC4A308FbAeD977648361fBAecFBCd380C7"; // big boy on Polygon
+  const sushiRouter = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
+  const quickRouter = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff";
+  const amount = Web3.utils.toWei("100000", "ether");
 
   beforeEach(async () => {
     Swapper = await ethers.getContractFactory("Swapper");
-    swapper = await Swapper.deploy(whaleAddress, sushiRouter); // set whale as parent
+    swapper = await Swapper.deploy(whaleAddress, [sushiRouter, quickRouter]); // set whale as parent
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
       params: [whaleAddress],
@@ -20,6 +21,7 @@ describe("Swapper", function () {
 
     WMATIC = new ethers.Contract("0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270", abi, ethers.provider);
     WBTC = new ethers.Contract("0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6", abi, ethers.provider); 
+    WETH = new ethers.Contract("0x7ceb23fd6bc0add59e62ac25578270cff1b9f619", abi, ethers.provider);
   });
   
   describe("swap", async () => {
@@ -29,7 +31,7 @@ describe("Swapper", function () {
       const initialWMATICBalance = await WMATIC.balanceOf(whaleAddress);
       const initialWBTCBalance = await WBTC.balanceOf(whaleAddress);
       
-      await swapper.connect(whale).swap(WMATIC.address, WBTC.address, amount, 0);
+      await swapper.connect(whale).swap([WMATIC.address, WBTC.address], amount, 0);
  
       const finalWMATICBalance = await WMATIC.balanceOf(whaleAddress);
       const finalWBTCBalance = await WBTC.balanceOf(whaleAddress);
@@ -41,7 +43,7 @@ describe("Swapper", function () {
 
     it("should leave no remaining funds in the swapper contract", async () => {
       await WMATIC.connect(whale).approve(swapper.address, amount);
-      await swapper.connect(whale).swap(WMATIC.address, WBTC.address, amount, 0);
+      await swapper.connect(whale).swap([WMATIC.address, WBTC.address], amount, 0);
 
       const finalSwapperWMATICBalance = await WMATIC.balanceOf(swapper.address);
       const finalSwapperWBTCBalance = await WBTC.balanceOf(swapper.address);
@@ -49,5 +51,29 @@ describe("Swapper", function () {
       expect(finalSwapperWMATICBalance.toString()).to.be.equal("0");
       expect(finalSwapperWBTCBalance.toString()).to.be.equal("0");
     });
+
+    it("finds optimal direct router", async () => {
+      await WMATIC.connect(whale).approve(swapper.address, amount);
+
+      const res = await swapper.connect(whale).findOptimalRouter(amount, [WMATIC.address, WETH.address, WBTC.address]);
+      
+      console.log(res[1].toString());
+    });
+
+    it("correctly registers routers", async () => {
+      await WMATIC.connect(whale).approve(swapper.address, amount);
+
+      const res0 = await swapper.connect(whale).router0(amount, [WMATIC.address, WBTC.address]);
+      const res1 = await swapper.connect(whale).router1(amount, [WMATIC.address, WBTC.address]);
+
+      console.log(res0[0], res1[0], res0[1].toString(), res1[1].toString());
+    });
+
+    /* it("finds optimal indirect router", async () => {
+      await WMATIC.connect(whale).approve(swapper.address, amount);
+
+      const res = await swapper.connect(whale).findOptimalRouter(amount, [WMATIC.address, WBTC.address]);
+      console.log(res); 
+    }); */
   });
 });
